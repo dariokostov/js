bf76e1334b56eb0c0a8ccca6bcdc5c2a34dfdfac
dariokostov <kostov.dario@gmail.com>
2019-02-28 00:37:49 +0100

    test2

--- DarioKostov-G2 #2/index ex.#2/index.js
+++ DarioKostov-G2 #2/index ex.#2/index.js
@@ -1,10 +1,5 @@
 var money = prompt("How much money you have?");
-if (money === "" || money === null || money <= 0){
-    alert("Please enter money");
-} else if (money <= 100){
-    alert("you can buy a beer");
-} else if (money <= 500){
-    alert ("you can buy a whiskey");
-} else {
-    alert ("you are rich");
-}
+if (money === "" || money === null || money <= 0) alert("Please enter money")
+else if (money <= 100) alert("you can buy a beer");
+else if (money <= 500) alert ("you can buy a whiskey");
+else alert ("you are rich");
